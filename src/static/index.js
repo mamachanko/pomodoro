1203dc16f7e9ebf91d7d31296cc9e91c7342ad8b
dilabuser <dilabuser@havoc-5.local>
2017-03-21 12:34:44 +0100

    play beep three times

--- src/static/index.js
+++ src/static/index.js
@@ -4,7 +4,22 @@ var Pomodoro = Elm.Pomodoro.fullscreen();
 
 Pomodoro.ports.playSound.subscribe(function(soundFile){
   console.log("playing sound: " + soundFile);
-  new Howl({
-    src: [soundFile]
-  }).play();
+  playSound(soundFile);
 });
+
+function playSound(soundFile) {
+  let playbackCounter = 0;
+  let numberOfPlaybacks = 3;
+
+  var howl = new Howl({
+    src: [soundFile],
+    loop: true,
+    autoplay: true,
+    onend: function() {
+      playbackCounter++;
+      if (playbackCounter === numberOfPlaybacks - 1) {
+        howl.loop(false);
+      }
+    }
+  });
+}
